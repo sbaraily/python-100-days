2603f70b91a3b345e0b9f83155fb182f6efc0988
Samir Baraily <35265993+sbaraily@users.noreply.github.com>
2022-12-06 14:37:35 +0000

    Day 6

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-print ("Hello\nWorld", 10, 200,501,51, sep="~", end="3")
-print("Hello\nWorld", 10, 100, 22, sep="~", end="4")
+#Day 6: Datatypes
+a = 1
+print(a)
+b = "Samir"
+print(b)
+c = True
+print(c)
+d = complex(8,2)
+print(d)
+print("The type of a is" , type(a))
+print("The type of b is" , type(b))
+print("The type of c is" , type(c))
+print("The type of d is" , type(d))
+#List is a collect of Data elements. List is mutable(changeable)
+list1 = [8, 3, -2, [-4,5], ["apple", "banana"]]
+#tuple is immutable (Cannot be changed)
+tuple1 = (("parrot", "sparrow"), ("lion", "tiger"))
+#Dictionary: Collection of Key:value pairs
+dict1 = {"name":"Sam", "age": 21, "canVote": True}
+print(list1)
+#Day 5
+# print ("Hello\nWorld", 10, 200,501,51, sep="~", end="3")
+# print("Hello\nWorld", 10, 100, 22, sep="~", end="4")
+
